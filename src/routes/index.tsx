356b5d97206d3bf39322d0daaa8502fdacabc93f
mac <mac@192.168.1.172>
2026-06-19 16:29:09 +0800

    移除LOGO容器justify-center，调整图片填充实现横向铺满侧边栏

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -102,7 +102,7 @@ function Index() {
             <img
               src={logoGif}
               alt="CanvasMath Logo"
-              className="h-full w-full object-contain"
+              className="h-full w-full object-contain scale-125"
             />
           </a>
         </div>
